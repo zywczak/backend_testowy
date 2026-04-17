8667eec117804e9cf6f830e4d06dc9fc8207425c
zywczak <123404938+zywczak@users.noreply.github.com>
2026-04-17 17:24:15 +0200

    update

---
+++ tracker/urls.py
@@ -0,0 +1,8 @@
+﻿from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("api/track", views.track_domain, name="track_domain"),
+    path("api/entries", views.get_entries, name="get_entries"),
+]
+
